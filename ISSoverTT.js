d19a250a2e115a8dfc69de68d34ffed8e1b58d19
Mary Karl <marymkarl@Mary-Karls-MacBook-Pro.local>
2014-08-02 17:49:04 -0400

    Initial commit

---
+++ ISSoverTT.js
@@ -0,0 +1,66 @@
+/*var http = require('http'); //add the http module
+var express = require('express');
+var app = express();
+var myServer = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.write("Hello");
+    response.end();
+}); //create a server
+
+myServer.listen(3000); //listen on port 3000
+console.log("Go to http:localhost:3000 on your browswer");
+console.log("Ctrl+C to quit");
+*/
+
+var moment = require('moment');
+var request = require('request');
+//var Twit = require('twit');
+// does Heroku require a response to http requests?
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+    res.send('Hello Express');
+}); //routing
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port 3000');
+});
+/*
+// twitter app info
+var T = new Twit({
+     consumer_key:          '',
+     consumer_secret:       '',
+     access_token:          '',
+     access_token_secret:   ''
+});
+
+var statement = 'testing';
+T.post('statuses/update', { status: statement}, function(err, reply) {
+       console.log("error: " + err);
+       console.log("reply: " + reply);
+       });
+*/
+
+var url = 'http://api.open-notify.org/iss-pass.json?lat=10.67&lon=-61.52&alt=25&n=1&callback=';
+
+(function whenISS () {
+    request({
+        url: url,
+        json: true
+    }, function(error, response, body) {
+        if(!error && response.statusCode == 200) {
+            //console.log(body) // print the json response
+            var date = body.response[0].risetime;
+            //console.log(date)
+            //console.log(moment.unix(date).toDate());
+            console.log(
+                'The ISS will be over T&T',
+                moment.unix(date).fromNow(),
+                '(' + moment.unix(date).zone('-04:00').format('h:mm a'),
+                'EDT)'
+            );
+        }
+    });
+    setTimeout(whenISS, 5000);
+ })();
+
---
+++ README.md
@@ -0,0 +1,34 @@
+ISSoverTT
+===============
+_by Keelan Chu For_
+
+A twitterbot.
+
+Requires [node](http://nodejs.org/). You also need a Twitter App access token, consumer key, and associated secrets: https://dev.twitter.com/apps/new
+
+To install dependencies and run:
+
+> npm install
+
+> node ISSoverTT.js
+    
+<br />    
+    
+## Licence
+
+Copyright (C) 2014 Keelan Chu For
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+You may contact the author at kkchufor@gmail.com
