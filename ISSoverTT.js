1428a3559d9e8a8a77c0976ebe1b9e789860d3c9
Keelan Chu For <kkchufor@gmail.com>
2014-08-02 20:09:02 -0400

    added git repo to package.json

--- ISSoverTT.js
+++ ISSoverTT.js
@@ -22,7 +22,7 @@ app.get('/', function(req, res) {
     res.send('Hello Express');
 }); //routing
 
-var server = app.listen(3000, function() {
+app.listen(3000, function() {
     console.log('Listening on port 3000');
 });
 /*
